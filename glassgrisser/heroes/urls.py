f594c6893c8f07bcb3a3388a5440f364ecb1a37e
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-04-28 23:26:42 +0300

    placeholder view for hero page

--- glassgrisser/heroes/urls.py
+++ glassgrisser/heroes/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path("", heroes_table)
+    path("", heroes_table),
+    path("<str:hero_name>", hero_page)
 ]
--- glassgrisser/heroes/views.py
+++ glassgrisser/heroes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def heroes_table(request):
     return HttpResponse("Placeholder")
+
+
+def hero_page(request, hero_name):
+    return HttpResponse(f"hero {hero_name} placeholder")
